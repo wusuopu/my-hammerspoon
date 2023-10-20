5d75ced4c0fad51a5f2a69abb70e3a24ac94a53b
wusuopu <admin@longchangjin.cn>
2023-10-20 15:09:49 +0800

    使用右shift键切换输入法

--- Spoons/KeyVolume.spoon/init.lua
+++ Spoons/KeyVolume.spoon/init.lua
@@ -12,10 +12,11 @@ obj.license = ""
 obj.logger = hs.logger.new('KeyVolume')
 
 obj.defaultHotkeys = {
+  -- 调节音量
    volume_down    = { {"shift", "alt"}, 'q' },
    volume_up      = { {"shift", "alt"}, 'w' },
    volume_mute    = { {"shift", "alt"}, 'e' },
-   switch_input_source = { {"shift", "alt"}, 'm' },     -- 切换输入法
+   -- switch_input_source = { {"shift", "alt"}, 'm' },     -- 切换输入法
 }
 
 function sendSystemKey(key)
@@ -54,13 +55,60 @@ function obj:bindHotkeys(mapping)
     volume_up = self.setVolumeUp,
     volume_down = self.setVolumeDown,
     volume_mute = self.setMute,
-    switch_input_source = switchInputSource
+    -- switch_input_source = switchInputSource
   }
  hs.spoons.bindHotkeysToSpec(action_to_method_map, mapping)
  return self
 end
 
+-- https://gist.github.com/amirrajan/ab15226a41f2ac0e4076faad954337f9
+-- ==========================================
+-- Right Shift
+-- ==========================================
+send_right_paren = false
+right_paren_last_mods = {}
+
+right_shift_key_handler = function()
+   send_right_paren = false
+end
+
+right_shift_key_timer = hs.timer.delayed.new(0.15, right_shift_key_handler)
+
+right_shift_handler = function(evt)
+  if evt:getKeyCode() ~= hs.keycodes.map["rightshift"] then
+    return
+  end
+  local new_mods = evt:getFlags()
+  if right_paren_last_mods["shift"] == new_mods["shift"] then
+    return false
+  end
+  if not right_paren_last_mods["shift"] then
+    right_paren_last_mods = new_mods
+    send_right_paren = true
+    right_shift_key_timer:start()
+  else
+    if send_right_paren then
+      -- 右shift 键切换输入法
+      switchInputSource()
+    end
+    right_paren_last_mods = new_mods
+    right_shift_key_timer:stop()
+  end
+  return false
+end
+
+right_shift_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, right_shift_handler)
+
+right_shift_other_handler = function(evt)
+   send_right_paren = false
+   return false
+end
+right_shift_other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, right_shift_other_handler)
+
 function obj:init()
+  -- right shift 功能
+  right_shift_tap:start()
+  right_shift_other_tap:start()
 end
 
 return obj
